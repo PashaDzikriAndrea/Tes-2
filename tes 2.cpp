e73dfee2741bafa17e366dd81d215b7fbcecc865
PashaDzikriAndrea <111964283+PashaDzikriAndrea@users.noreply.github.com>
2022-10-10 14:33:57 +0700

    Add files via upload

---
+++ tes 2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    
+cout << "Rumus"<<endl;
+    cout << "Masukan angka Pertama = ";
+    int angka1;
+    cin >> angka1;
+    
+    cout << "Masukan angka kedua = ";
+    int angka2;
+    cin >> angka2;
+   
+    cout << " masukan angka ketiga = ";
+    int angka3;
+    cin >> angka3;
+    
+    int hasil;
+    hasil = (angka1 + angka2)/ angka3;
+    cout << " Hasil rumus = "<< hasil ;
+    
+    }
